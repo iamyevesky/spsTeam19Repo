1cdc6e3c137e4efe145981a020b27a1c21cfe1cb
Sena Yevenyo <yevesky2020@gmail.com>
2020-04-24 00:23:31 -0400

    Implement BulletinPost feature

--- src/main/java/com/google/sps/classes/BulletinPost.java
+++ src/main/java/com/google/sps/classes/BulletinPost.java
@@ -1,7 +1,8 @@
-package com.java.sps.classes;
+package com.google.sps.classes;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
@@ -24,29 +25,47 @@ public final class BulletinPost{
     private final Department department;
     private final User user;
     private final String body;
-    private Key key;
+    private final Timestamp date;
 
     private BulletinPost(User user, String title, String body, Department department){
         this.title = title;
         this.user = user;
-        this.board = board;
+        this.department = department;
         this.body = body;
-        this.date = new Date();
+        this.date = Timestamp.now();
     }
 
     private void saveToDataBase(){
         Entity post = new Entity("BulletinPost");
         post.setProperty("title", this.title);
         post.setProperty("body", this.body);
-        post.setProperty("timestamp", this.date);
-        post.setProperty("boardID", this.board.getKey());
-        post.setProperty("userID", this.user.getKey());
+        post.setProperty("timestamp", this.date.toDate());
+        post.setProperty("departmentID", KeyFactory.stringToKey(this.department.getKey()));
+        post.setProperty("userID", KeyFactory.stringToKey(this.user.getKey()));
         datastore.put(post);
     }
 
-    public static void addPostToDatabase(User user, String title, String body, BulletinBoard board){
-        BulletinPost newPost = new BulletinPost(user, title, body, board);
+    private static BulletinPost getPost(Entity entity) throws EntityNotFoundException{
+        return new BulletinPost(User.getUser((Key) entity.getProperty("userID")), (String) entity.getProperty("title"), (String) entity.getProperty("body"), Department.getDepartment((Key) entity.getProperty("departmentID")));
+    }
+
+    public static void addPostToDatabase(User user, String title, String body, Department department){
+        BulletinPost newPost = new BulletinPost(user, title, body, department);
         newPost.saveToDataBase();
     }
-    */
+
+    public static ArrayList<BulletinPost> getPosts(Department department) throws EntityNotFoundException{
+        Query query = 
+        new Query("BulletinPost")
+        .setFilter(new Query.
+        FilterPredicate("departmentID", Query.FilterOperator.EQUAL, KeyFactory.stringToKey(department.getKey())))
+        .addSort("timestamp", SortDirection.ASCENDING);
+        
+        PreparedQuery result = datastore.prepare(query);
+        ArrayList<BulletinPost> output = new ArrayList<BulletinPost>();
+        for (Entity entity : result.asIterable()){
+            output.add(BulletinPost.getPost(entity));    
+        }
+        return output;
+    }
 }

f5571302697e47557e403b594f42d8825b3e245d
Alan <10alanm@gmail.com>
2020-05-18 21:44:04 -0400

    Create form to change name in profile

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -7,7 +7,7 @@ function loadProfile() {
         console.log(jsonObject);
         console.log(typeof jsonObject)
         setUpUserPage(jsonObject);
-        loadClasses();
+        //loadClasses();
     });
 }
 
@@ -32,22 +32,12 @@ function setUpUserPage(json){
     email.innerText = String(jsonData.user.email);
     var spacer = document.createElement("BR");
 
-    //change nickname link
-    var changeNicknameP = document.createElement("p");
-    changeNicknameP.innerText = "Change your nickname: ";
-    var anchor = document.createElement("A");
-    var link = document.createTextNode("here");
-    anchor.setAttribute("href", "#");
-    anchor.appendChild(link);
-    changeNicknameP.appendChild(anchor);
-
     var profileContainer = document.getElementById("profile-container");
     profileContainer.appendChild(name);
     profileContainer.appendChild(uni);
     profileContainer.appendChild(classification);
     profileContainer.appendChild(email);
     profileContainer.appendChild(spacer);
-    profileContainer.appendChild(changeNicknameP);
 }
 
 function loadClasses() {

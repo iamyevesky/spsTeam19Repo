85094b79ffb31f2b791c50165a1e4284bdd19caf
Alan <10alanm@gmail.com>
2020-05-26 23:34:53 -0400

    UI to join chat

--- src/main/webapp/exploreScript.js
+++ src/main/webapp/exploreScript.js
@@ -1,116 +1,77 @@
-var course1 = {
-    name: "CS 1301",
-    department: "College of Computing",
-    college: "Georgia Tech",
-};
-
-var course2 = {
-    name: "CS 1331",
-    department: "College of Computing",
-    college: "Georgia Tech",
-};
-var course3 = {
-    name: "Math 2550",
-    department: "Mathematics",
-    college: "Georgia Tech",
-};
-var course4 = {
-    name: "LMC 1301",
-    department: "Arts",
-    college: "UGA",
-};
-
-var course5 = {
-    name: "LMC 3000",
-    department: "Arts",
-    college: "UGA",
-};
-var course6 = {
-    name: "Film 2120",
-    department: "Film",
-    college: "UGA",
-};
-
-var exampleBackend = [course1, course2, course3, course4, course5, course6];
-
-
-function load_departments() {
-
-    //load departments based on college name
-    var universityValue = document.getElementById("inputUni").value;
-    var arrDepartments = getDepartments(universityValue);
-    var string="<option></option>";       
-    for(i = 0; i < arrDepartments.length; i++) {
-        string += "<option value='" + arrDepartments[i] + "'>" + arrDepartments[i] + "</option>";
-    }
-    document.getElementById("departmentOutput").innerHTML = string;
-    document.getElementById("courseOutput").innerHTML = "";
-
-
-}
-
-function load_courses() {
-    //load courses based on department name
-    var departmentValue = document.getElementById("departmentOutput").value;
-    var arrCourses = getCourses(departmentValue);
-    var courseStrings = "<option></option>"; 
-    for(i = 0; i < arrCourses.length; i++) {
-        courseStrings += "<option value='" + arrCourses[i] + "'>" + arrCourses[i] + "</option>";
-    }
-    document.getElementById("courseOutput").innerHTML = courseStrings;
-    console.log(document.getElementById("courseOutput"));
-    
-}
-
-//load based on backend 
-function createUniOptions() {
-    var uniDropdown = document.getElementById("inputUni");
-    var universityStrings = "";
-    var allUniversities = [];
-    for (i = 0; i < exampleBackend.length; i++) {
-        currUniName = exampleBackend[i].college;
-
-        //check to see if curr university name has already been added to dropdown
-        if (!allUniversities.includes(currUniName)) {
-            allUniversities.push(currUniName);
-            universityStrings += "<option value='" + currUniName + "'>" + currUniName + "</option>";
+function getAvailableChats() {
+    fetch("/createChat").then(response => response.json()).then(object =>
+        {
+            console.log(object);
+            appendCollegeChat(object);
+            createTitle(object);
         }
-        
-    }
-    uniDropdown.innerHTML += universityStrings;
+    );
 }
 
-function getDepartments(uniName) {
-
-    var allDepartments = [];
-    for (i = 0; i < exampleBackend.length; i++) {
-        //only check departments of same college
-        if (exampleBackend[i].college == uniName) {
-            currDepName = exampleBackend[i].department;
-
-            //check to see if curr department name has already been added to list
-            if (!allDepartments.includes(currDepName)) {
-                allDepartments.push(currDepName);
-            }
-        }
-        
-    }
-    return allDepartments;
+function createTitle(jsonObj) {
+    userUniName = jsonObj.user.college.name;
+    var uniTitle = document.getElementById("uniTitle");
+    uniTitle.innerText = userUniName;
+    uniTitle.style.textAlign = "center";
 }
-function getCourses(departmentName) {
 
-    var allCourses = [];
-    for (i = 0; i < exampleBackend.length; i++) {
-        //only check courses of same department
-        if (exampleBackend[i].department == departmentName) {
-            currCourseName = exampleBackend[i].name;
+function appendCollegeChat(jsonObj) {
+    var chatInitial = document.getElementById("chat-container");
+    var chatContainer = document.createElement("div");
+    chatContainer.classList.add("row");
+    chatInitial.appendChild(chatContainer);
+    for (i = 0; i < jsonObj.chats.length; i++){
+        var colContainer = document.createElement("div");
+        colContainer.classList.add("column");
+        var singleCard = document.createElement("div");
+        singleCard.classList.add("card");
+        colContainer.appendChild(singleCard);
 
-            //check to see if curr courses name has already been added to list
-            if (!allCourses.includes(currCourseName)) {
-                allCourses.push(currCourseName);
-            }
-        }
-        
+        var cardBody = document.createElement("div");
+        cardBody.className = "card-body";
+        singleCard.appendChild(cardBody);
+
+        const currChat = jsonObj.chats[i];
+        createBody(cardBody, currChat);
+        chatContainer.appendChild(colContainer);
     }
-    return allCourses;
+}
+
+function createBody(bodyOutline, currPost) {
+    const chatKey = currPost.key + "";
+    const chatName = currPost.name + "";
+
+    const form = document.createElement("FORM");
+    form.action = "/updateChat";
+    form.method = "POST";
+
+    //chat title
+    const chatNameTitle = document.createElement("h5");
+    chatNameTitle.innerText = chatName;
+    chatNameTitle.className = "card-title";
+    chatNameTitle.style.textAlign = "center";
+
+    //input element, Submit button
+    var sDiv = document.createElement("div");
+    sDiv.style.textAlign = "center";
+
+    var s = document.createElement("input");
+    s.setAttribute('type',"submit");
+    s.setAttribute('value',"Join Chat!");
+    s.style.color = "white";
+    s.style.backgroundColor = "#05728f";
+    s.style.marginTop = "25px";
+    s.classList.add("btn");
+    sDiv.appendChild(s);
+
+    //hidden attributes needed for servlet
+    const hidden = document.createElement("INPUT");
+    hidden.setAttribute("type", "hidden");
+    hidden.setAttribute("name", "chatKey");
+    hidden.setAttribute("value", chatKey);
+    form.appendChild(hidden);
+
+    form.appendChild(chatNameTitle);
+    form.appendChild(sDiv);
+    bodyOutline.appendChild(form);
 }
